3c7a560b17f5c5d9baa8cbcf7e0bd90dedbfd5fb
Rushil Gupta <rushil@ucsb.edu>
2024-10-09 11:40:23 -0700

    rg - updated with team name and members

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -40,13 +40,13 @@ public class Developer {
     
     public static Team getTeam() {
         // TODO: Change this to your team name
-        Team team = new Team("f24-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("f24-02");
+        team.addMember("Edwin");
+        team.addMember("Jakob");
+        team.addMember("Jeffery");
+        team.addMember("Leo");
+        team.addMember("Rushil");
+        team.addMember("Ved");  
         return team;
     }
 }
